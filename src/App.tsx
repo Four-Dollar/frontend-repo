f7f966cb310c98242235d0cf4f5977fc96519b2a
pione3r <jhyong0626@gmail.com>
2022-11-21 13:04:13 +0900

    conf: 프로젝트 초기 설정 -3
    - styled-components 설정
    - tailwindcss 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TestDiv = styled.div`
+	background-color: tomato;
+`;
 
 function App() {
-	return <div>Hello world</div>;
+	return (
+		<>
+			<TestDiv>Hello world</TestDiv>
+			<h1 className="text-3xl font-bold underline text-red-500">
+				Hello world!
+			</h1>
+		</>
+	);
 }
 
 export default App;
