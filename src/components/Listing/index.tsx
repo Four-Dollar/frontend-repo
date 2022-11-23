0aad37288170ab58aea0782298e4e9219f4de8d3
pione3r <jhyong0626@gmail.com>
2022-11-23 18:17:03 +0900

    feat: 상품 설명 컴포넌트 onChange 이벤트 추가
    onChangeDescription
    - 바이트 수 set state

--- src/components/Listing/index.tsx
+++ src/components/Listing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.section`
@@ -59,14 +59,23 @@ const DescriptionByteLimit = styled.div`
 `;
 
 export function Listing() {
+	const [byte, setByte] = useState(0);
+
+	const onChangeDescription = (
+		event: React.ChangeEvent<HTMLTextAreaElement>,
+	) => {
+		setByte(event.target.value.length);
+	};
+
 	return (
 		<Container>
 			<TitleInput placeholder="제목을 입력해주세요" />
 			<DescriptionInputContainer>
 				<DescriptionInput
 					placeholder="사진 및 상품에 대한 자세한 게시글 내용을 작성해주세요.&#10;(가품 및 판매금지 물품은 게시가 제한될 수 있습니다.)"
+					onChange={onChangeDescription}
 				></DescriptionInput>
-				<DescriptionByteLimit>0 / 1000</DescriptionByteLimit>
+				<DescriptionByteLimit>{byte} / 1000</DescriptionByteLimit>
 			</DescriptionInputContainer>
 		</Container>
 	);
