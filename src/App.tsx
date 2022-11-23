591e7c860a9da6e8452a5cc3f24e97df855891d4
pione3r <jhyong0626@gmail.com>
2022-11-23 14:38:37 +0900

    feat: 헤더 컴포넌트 UI 구현 완료
    - 로고 추가
    - 임시로 유저 아이디 입력하는 element 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
+import { Header } from 'components';
 import React from 'react';
 import styled from 'styled-components';
-
-const TestDiv = styled.div`
-	background-color: tomato;
-`;
 
 function App() {
 	return (
 		<>
-			<TestDiv>Hello world</TestDiv>
-			<h1 className="text-3xl font-bold underline text-red-500">
-				Hello world!
-			</h1>
+			<Header />
 		</>
 	);
 }
