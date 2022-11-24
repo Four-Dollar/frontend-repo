d11b70a9a48a8f712670a5ab828f95e7ce25c119
pione3r <jhyong0626@gmail.com>
2022-11-24 16:35:50 +0900

    feat: 상품 등록 컴포넌트 변경
    - 상품 등록 제목칸 높이 자동 변경 함수 추가
    - 제목과 설명글 구분하는 수평선 추가
    - 상품 등록 제목 입력창 감싸는 컴포넌트 추가

--- src/components/Listing/index.tsx
+++ src/components/Listing/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.section`
@@ -10,13 +10,17 @@ const Container = styled.section`
 	height: 90vh;
 `;
 
+const TitleInputContainer = styled.div`
+	width: 546px;
+	height: fit-content;
+`;
+
 const TitleInput = styled.textarea`
 	font-weight: 600;
 	font-size: 48px;
-	width: 546px;
-	height: auto;
 	resize: none;
 	outline: none;
+	overflow: hidden;
 	color: #111111;
 
 	&::placeholder {
@@ -27,7 +31,6 @@ const TitleInput = styled.textarea`
 const DescriptionInputContainer = styled.div`
 	display: flex;
 	flex-direction: column;
-	width: 100%;
 `;
 
 const DescriptionInput = styled.textarea`
@@ -54,19 +57,36 @@ const DescriptionByteLimit = styled.div`
 	color: #a2a2a2;
 `;
 
+const Horizon = styled.hr`
+	width: 5vw;
+	height: 1vh;
+	background-color: #b7b7b7;
+`;
+
 export function Listing() {
 	const [title, setTitle] = useState('');
 	const [titleByteCheck, setTitleByteCheck] = useState(0);
 	const [description, setDescription] = useState('');
 	const [descriptionByteCheck, setDescriptionByteCheck] = useState(0);
 
+	const titleInput = useRef<HTMLTextAreaElement>(null);
+
 	const onChangeTitle = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+		autoTitleHeightChange();
+
 		if (titleByteCheck < 50) {
 			setTitle(event.target.value);
 			setTitleByteCheck(event.target.value.length);
 		} else if (titleByteCheck >= 50) {
 			setTitle((prev) => prev.substring(0, 50));
 			setTitleByteCheck(49);
+		}
+	};
+
+	const autoTitleHeightChange = () => {
+		if (titleInput.current !== null) {
+			titleInput.current.style.height = 'auto';
+			titleInput.current.style.height = `${titleInput.current.scrollHeight}px`;
 		}
 	};
 
@@ -84,14 +104,18 @@ export function Listing() {
 
 	return (
 		<Container>
-			<TitleInput
-				id="title"
-				placeholder="제목을 입력해주세요"
-				value={title}
-				onChange={onChangeTitle}
-				spellCheck={false}
-				rows={1}
-			/>
+			<TitleInputContainer>
+				<TitleInput
+					id="title"
+					placeholder="제목을 입력해주세요"
+					value={title}
+					onChange={onChangeTitle}
+					spellCheck={false}
+					rows={1}
+					ref={titleInput}
+				/>
+			</TitleInputContainer>
+			<Horizon />
 			<DescriptionInputContainer>
 				<DescriptionInput
 					placeholder="사진 및 상품에 대한 자세한 게시글 내용을 작성해주세요.&#10;(가품 및 판매금지 물품은 게시가 제한될 수 있습니다.)"
