81113590f93de4122a1c3722cdeb67b3663c28b0
pione3r <jhyong0626@gmail.com>
2022-11-25 16:40:36 +0900

    fix: merge 에러 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
-import DragScaleBar from 'components/dragBar/DragScaleBar';
-import InputPrice from 'components/InputWithReg/InputPrice';
 import React from 'react';
 import styled from 'styled-components';
 
-const TestDiv = styled.div`
-	background-color: tomato;
-`;
+import DragScaleBar from 'components/dragBar/DragScaleBar';
+import { Header } from 'components';
 
 function App() {
 	return (
-		// <>
-		// 	<TestDiv>Hello world</TestDiv>
-		// 	<h1 className="text-3xl font-bold underline text-red-500">
-		// 		Hello world!
-		// 	</h1>
-		// </>
 		<>
 			<DragScaleBar></DragScaleBar>
-			<InputPrice></InputPrice>
+			<Header />
 		</>
 	);
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as Logo } from 'assets/images/Logo.svg';
+
+const Container = styled.header`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	padding: 12px;
+`;
+const LogoContainer = styled.div`
+	width: fit-content;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 4px;
+`;
+const LogoText = styled.div`
+	font-family: 'Poppins';
+	font-style: normal;
+	font-weight: 900;
+	font-size: 22px;
+	line-height: 42px;
+	letter-spacing: 0.05em;
+	color: #000000;
+`;
+const UserIdInput = styled.input`
+	font-family: 'Poppins';
+	font-style: normal;
+	font-weight: 500;
+	font-size: 18px;
+	line-height: 36px;
+	text-align: justify;
+	margin-left: auto;
+	color: #a2a2a2;
+`;
+
+export function Header() {
+	return (
+		<Container>
+			<LogoContainer>
+				<Logo />
+				<LogoText>FourDollar</LogoText>
+			</LogoContainer>
+			<UserIdInput placeholder="UserId 입력 (임시)" />
+		</Container>
+	);
+}
