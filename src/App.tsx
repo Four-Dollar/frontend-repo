0c117d290b39a9f900a94352532a3da43e14f4f6
pione3r <jhyong0626@gmail.com>
2022-11-26 21:21:10 +0900

    fix: 이미지 업로드 컴포넌트 merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
+import { Header } from 'components';
 import React from 'react';
 import styled from 'styled-components';
-
-import DragScaleBar from 'components/dragBar/DragScaleBar';
-import { Header } from 'components';
 
 function App() {
 	return (
 		<>
-			<DragScaleBar></DragScaleBar>
 			<Header />
 		</>
 	);
